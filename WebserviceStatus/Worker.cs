43df0ec2271474d2bf7e146a94f9c39d794c91ac
x340348 <Tty_Erick.Alencar@mercantil.com.br>
2022-06-15 23:59:48 -0300

    First commit

---
+++ WebserviceStatus/Worker.cs
@@ -0,0 +1,49 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient.Memcached;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebserviceStatus
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private HttpClient client;
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            client = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            client.Dispose();
+            return base.StopAsync(cancellationToken);
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var result = await client.GetAsync("https://iamtincorey.com");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("The website is up. Status Code {StatusCode}", result.StatusCode);
+                }
+                else
+                {
+                    _logger.LogError("The website is down. Status Code {StatusCode}", result.StatusCode);
+                }
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
